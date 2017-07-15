7e0a2e93cd78086150e7f2485120aad53dfc35b1
Ziyue Yang <yzylivezh@hotmail.com>
2017-07-15 16:58:11 +0800

    monitoring method extraction

--- dsm_patcher/scripts/trace_monitor.py
+++ dsm_patcher/scripts/trace_monitor.py
@@ -6,23 +6,91 @@ import os
 import argparse
 import subprocess
 
-from utils import ADBException, ADB
+from utils import ADBException, ADB, java_method_convert
+
+
+def get_monitoring_methods(trace_item_list):
+    ret_list = []
+
+    for trace_item in trace_item_list:
+        fields = trace_item.split()
+        if fields[1].endswith(")Z") or\
+           fields[1].endswith(")B") or\
+           fields[1].endswith(")C") or\
+           fields[1].endswith(")S") or\
+           fields[1].endswith(")I") or\
+           fields[1].endswith(")J"):
+            ret_list.append(java_method_convert(trace_item))
+    return set(ret_list)
 
 
 def monitor_func(device_id, apk_path_list, droidbot_out_dir,
-                 trace_comparator_output_dir, output_dir, timeout):
+                 trace_comparator_out_dir, output_dir, timeout, is_emulator):
     """
     test apks on the assigned vm/device
     """
     for apk_path in apk_path_list:
-        full_output_dir = "%s/%s/%s" % (output_dir, device_id,
-                                        apk_path.split("/")[-1][:-len(".apk")])
+        apk_file_name = apk_path.split("/")[-1][:-len(".apk")]
+        full_output_dir = "%s/%s/%s" % (output_dir, device_id, apk_file_name)
         if os.system("mkdir -p %s" % full_output_dir):
             print "failed mkdir -p %s" % full_output_dir
             continue
 
-        print full_output_dir
-        print apk_path_list
+        app_droidbot_out_dir = "%s/%s" % (droidbot_out_dir, apk_file_name)
+
+        # get package name by dumpsys_package file name
+        package_name = [x for x in os.walk(app_droidbot_out_dir).next()[2]
+                        if x.startswith("dumpsys")][0][len("dumpsys_package_"):-len(".txt")]
+
+        # get monitoring method list from trace_comparator_output_dir
+        # tid: methods
+        monitoring_methods_list = []
+        comparator_result_paths = ["%s/%s" % (trace_comparator_out_dir, x)
+                                   for x in os.walk(trace_comparator_out_dir).next()[2]
+                                   if x.startswith(apk_file_name)]
+        for comparator_result_path in comparator_result_paths:
+            with open(comparator_result_path, "r") as comparator_result_file:
+                comparator_result = json.load(comparator_result_file)
+                if is_emulator:
+                    unmatched_idx = "emulator"
+                    matched_idx = "emu_api"
+                else:
+                    unmatched_idx = "real_device"
+                    matched_idx = "real_api"
+
+                monitoring_methods = set()
+                for thread_info in comparator_result["unmatched_threads"][unmatched_idx]:
+                    unmatched_methods = get_monitoring_methods(thread_info["api"])
+                    monitoring_methods = monitoring_methods.union(unmatched_methods)
+                for thread_info in comparator_result["matched_threads"]:
+                    if thread_info[matched_idx] is not None:
+                        matched_methods = get_monitoring_methods(thread_info[matched_idx])
+                        monitoring_methods = monitoring_methods.union(matched_methods)
+                monitoring_methods_list.append(sorted(list(monitoring_methods)))
+                print len(monitoring_methods_list[-1])
+
+        # intialize ADB
+        adb = ADB(device_id)
+
+        # install the app
+        # print adb.run_cmd(["install", "-r", "-g", apk_path])
+
+        # set debug-app
+
+        # start app
+
+        # fire events
+            # jdwp attach (multiple threads)
+            # freeze, hack debug detection method, resume until last method
+            # jdwp un-attach
+            # wait interval seconds
+
+        # uninstall the app
+        # print adb.run_cmd(["uninstall", package_name])
+
+        with open("%s/monitoring_methods.json" % (full_output_dir), "w") as monitoring_methods_file:
+            json.dump(monitoring_methods_list, monitoring_methods_file, indent=2)
+
 
 def run(config_json_path):
     """
@@ -34,9 +102,10 @@ def run(config_json_path):
     real_device_droidbot_out_dir = os.path.abspath(config_json["real_device_droidbot_out_dir"])
     emulator_droidbot_out_dir = os.path.abspath(config_json["emulator_droidbot_out_dir"])
     trace_comparator_out_dir = os.path.abspath(config_json["trace_comparator_out_dir"])
-    real_device_id = os.path.abspath(config_json["real_device_id"])
-    emulator_id = os.path.abspath(config_json["emulator_id"])
     output_dir = os.path.abspath(config_json["output_dir"])
+
+    real_device_id = config_json["real_device_id"]
+    emulator_id = config_json["emulator_id"]
 
     apk_dir = os.path.abspath(config_json["apk_dir"])
     apk_path_list = ["%s/%s" % (apk_dir, x) for x in
@@ -47,10 +116,10 @@ def run(config_json_path):
     # start monitors
     real_device_monitor = Process(target=monitor_func, args=(
         real_device_id, apk_path_list, real_device_droidbot_out_dir,
-        trace_comparator_out_dir, output_dir, timeout))
+        trace_comparator_out_dir, output_dir, timeout, False))
     emulator_monitor = Process(target=monitor_func, args=(
         emulator_id, apk_path_list, emulator_droidbot_out_dir,
-        trace_comparator_out_dir, output_dir, timeout))
+        trace_comparator_out_dir, output_dir, timeout, True))
 
     real_device_monitor.start()
     emulator_monitor.start()
--- dsm_patcher/scripts/utils.py
+++ dsm_patcher/scripts/utils.py
@@ -87,3 +87,33 @@ class ADB():
 
     def touch(self, x, y):
         self.shell("input tap %d %d" % (x, y))
+
+def java_method_convert(short_sig):
+    basic_type_mapping = {
+        "Z": "boolean",
+        "B": "byte",
+        "C": "char",
+        "S": "short",
+        "I": "int",
+        "J": "long",
+        "F": "float",
+        "D": "double"
+    }
+    fields = short_sig.split()
+    idx = 1
+    array_depth = 0
+    parsed_paras = []
+    while fields[1][idx] != ")":
+        if fields[1][idx] == "L":
+            class_end_idx = idx
+            while fields[1][class_end_idx] != ";":
+                class_end_idx += 1
+            parsed_paras.append(fields[1][idx + 1:class_end_idx].replace("/", ".") + array_depth * "[]")
+            idx = class_end_idx
+        elif fields[1][idx] == "[":
+            array_depth += 1
+        else:
+            parsed_paras.append(basic_type_mapping[fields[1][idx]] + array_depth * "[]")
+            array_depth = 0
+        idx += 1
+    return "%s(%s)" % (fields[0], ",".join(parsed_paras))
